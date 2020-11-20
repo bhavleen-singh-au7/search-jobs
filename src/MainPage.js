d6360e4698205a72f30a2486424b0ea00a6b024e
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-11-20 13:16:31 +0530

    Geting cors error from api

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,12 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import JobCard from "./components/Jobcard/JobCard";
 import NavBar from "./components/Navbar/NavBar";
+import axios from "axios";
 
 const MainPage = () => {
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        "https://jobs.github.com/positions.json",
+        {
+          headers: {
+            "Access-Control-Allow-Origin":
+              "http://localhost:3000",
+            "Access-Control-Allow-Credentials": "true",
+          },
+        }
+      );
+
+      console.log(result);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <Fragment>
       <NavBar />
-      
+
       <JobCard
         location="delhi"
         type="full time"
